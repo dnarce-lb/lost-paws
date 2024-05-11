cee7bd3ed6fc1b4bf94da22ec8c71f17da955d8b
Mariano Troncoso <mtroncoso@litebox.ai>
2024-05-10 21:51:30 -0300

    feat: Add logic to upload images

--- apps/front/assets/icons/index.ts
+++ apps/front/assets/icons/index.ts
@@ -2,3 +2,6 @@ export { default as LogoIcon } from './logo.svg';
 export { default as BurguerMenu } from './burger-menu.svg';
 export { default as ArrowDown } from './arrow-down.svg';
 export { default as ArrowLeft } from './arrow-left.svg';
+export { default as CheckMark } from './check-mark.svg';
+export { default as Plus } from './plus.svg';
+export { default as Close } from './close-circle.svg';
--- apps/front/components/Form/Form.tsx
+++ apps/front/components/Form/Form.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 'use client';
 
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { ArrowDown, ArrowLeft } from '@/assets/icons';
+import { ArrowDown, ArrowLeft, CheckMark, Close, Plus } from '@/assets/icons';
 
 enum StepsEnum {
   UBICACION = 'Ubicación',
@@ -33,15 +35,19 @@ const StepperLabels = ({ currentStep }: { currentStep: Step }) => {
 
         const isCurrentStep = number === currentStep.number;
 
+        const showSuccessIcon = number < currentStep.number;
+
         const stepNumberClassName = 'rounded-full w-6 h-6 flex items-center justify-center'.concat(
           isCurrentStep ? ' border-2 border-mainBlue' : ' border border-mainBlack/10'
         );
 
-        const stepNameClassName = 'mt-3'.concat(isCurrentStep ? ' text-mainBlack' : ' text-mainBlack/60');
+        const stepNameClassName = 'mt-3'.concat(
+          isCurrentStep || showSuccessIcon ? ' text-mainBlack' : ' text-mainBlack/60'
+        );
 
         return (
           <div key={number} className='flex flex-col items-center'>
-            <div className={stepNumberClassName}>{number}</div>
+            {showSuccessIcon ? <Image src={CheckMark} alt='' /> : <div className={stepNumberClassName}>{number}</div>}
             <div className={stepNameClassName}>{name}</div>
           </div>
         );
@@ -55,7 +61,7 @@ type Step = {
   name: StepsEnum;
 };
 
-const UbicacionStep = ({ onHideStepperLabels }: { onHideStepperLabels: (params: boolean) => void }) => {
+const UbicacionStep = ({ onHideStepperLabels }: { onHideStepperLabels?: (params: boolean) => void }) => {
   const [isShowingLocations, setisShowingLocations] = useState<boolean>(false);
 
   return !isShowingLocations ? (
@@ -77,7 +83,7 @@ const UbicacionStep = ({ onHideStepperLabels }: { onHideStepperLabels: (params: 
               className='bg-white border rounded-full py-3 px-4 text-mainBlack/60 w-full'
               type='button'
               onClick={() => {
-                onHideStepperLabels(false);
+                onHideStepperLabels?.(false);
                 setisShowingLocations(true);
               }}>
               <div className=' flex flex-start '>Cerca de</div>
@@ -106,7 +112,7 @@ const UbicacionStep = ({ onHideStepperLabels }: { onHideStepperLabels: (params: 
               height={24}
               alt=''
               onClick={() => {
-                onHideStepperLabels(true);
+                onHideStepperLabels?.(true);
                 setisShowingLocations(false);
               }}
             />
@@ -123,16 +129,142 @@ const UbicacionStep = ({ onHideStepperLabels }: { onHideStepperLabels: (params: 
   );
 };
 
-const getStepConfifg = (onHideStepperLabels: (params: boolean) => void) => {
+const MAX_IMAGES_LIMIT = 3;
+
+const plusIcons = [
+  {
+    id: 1,
+    src: Plus,
+  },
+  {
+    id: 2,
+    src: Plus,
+  },
+  {
+    id: 3,
+    src: Plus,
+  },
+];
+
+const FotosStep = () => {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  const handleFileChange: React.ChangeEventHandler<HTMLInputElement> | undefined = e => {
+    const files = Array.from(e.target.files as Iterable<File> | ArrayLike<File>);
+
+    if (Array.from(files).length > MAX_IMAGES_LIMIT) {
+      e.preventDefault();
+      return;
+    }
+
+    setSelectedFiles(Array.from(files));
+  };
+
+  const amountOfPlusIcons = MAX_IMAGES_LIMIT - selectedFiles.length;
+
+  const handleAddFiles: React.ChangeEventHandler<HTMLInputElement> | undefined = e => {
+    const files = Array.from(e.target.files as Iterable<File> | ArrayLike<File>);
+
+    if (Array.from(files).length > amountOfPlusIcons) {
+      e.preventDefault();
+      return;
+    }
+
+    setSelectedFiles(prev => {
+      const newFiles = files.filter(file => !prev.some(prevFile => prevFile.name === file.name));
+
+      return [...prev, ...newFiles];
+    });
+  };
+
+  const handleRemoveImage = (fileName: string) => {
+    setSelectedFiles(prev => prev.filter(file => file.name !== fileName));
+  };
+
+  const areImagesSelected = selectedFiles.length > 0;
+
+  return (
+    <div className='bg-white text-mainBlack rounded-3xl p-8 flex flex-col'>
+      <div>
+        <span className='text-lg font-semibold'>Subí fotos</span>
+      </div>
+      <div className='text-mainBlack/60'>
+        <div>
+          Subí fotos con buena luz y donde se pueda apreciar bien a tu mascota. ¡Nos va a ayudar a encontrarla más
+          rápido
+        </div>
+        <div className='mt-4'>Podés subir hasta {MAX_IMAGES_LIMIT} fotos.</div>
+      </div>
+      {!areImagesSelected && (
+        <label className='bg-white  w-full' htmlFor='myfile'>
+          <input type='file' accept='image/*' hidden multiple id='myfile' onChange={handleFileChange} />
+          <div className='w-full mt-6 border border-mainBlue rounded-full text-mainBlue flex items-center justify-center h-10'>
+            Subir fotos
+          </div>
+        </label>
+      )}
+      <div className='mt-6 flex gap-2'>
+        {selectedFiles.map(file => (
+          <div key={file.name} className='relative'>
+            <Image
+              className='absolute top-[-10px] right-[-10px]'
+              src={Close}
+              width={24}
+              height={24}
+              alt=''
+              onClick={() => handleRemoveImage(file.name)}
+            />
+            <Image
+              style={{
+                objectFit: 'cover',
+                borderRadius: '12px',
+              }}
+              src={URL.createObjectURL(file)}
+              alt=''
+              className='object-cover w-20 h-20'
+              width={80}
+              height={80}
+            />
+          </div>
+        ))}
+        {areImagesSelected &&
+          plusIcons.slice(MAX_IMAGES_LIMIT - amountOfPlusIcons).map(plusIcon => (
+            <div key={plusIcon.id}>
+              <label className='bg-white  w-full' htmlFor={`myfile-${plusIcon.id}`}>
+                <input
+                  type='file'
+                  accept='image/*'
+                  hidden
+                  multiple
+                  id={`myfile-${plusIcon.id}`}
+                  onChange={handleAddFiles}
+                />
+                <div
+                  className='border border-mainBlue flex items-center justify-center h-20'
+                  style={{
+                    minWidth: '80px',
+                    borderRadius: '12px',
+                  }}>
+                  <Image src={plusIcon.src} alt='' width={36} height={36} />
+                </div>
+              </label>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const getStepConfifg = (onHideStepperLabels?: (params: boolean) => void) => {
   return {
     [StepsEnum.UBICACION]: <UbicacionStep onHideStepperLabels={onHideStepperLabels} />,
-    [StepsEnum.FOTOS]: <div>Fotos</div>,
+    [StepsEnum.FOTOS]: <FotosStep />,
     [StepsEnum.INFORMACION]: <div>Información</div>,
   };
 };
 
 const Form: React.FC = () => {
-  const [currentStep, setCurrentStep] = useState<Step>(steps[0]);
+  const [currentStep, setCurrentStep] = useState<Step>(steps[1]);
 
   const [showStepperLabels, setShowStepperLabels] = useState<boolean>(true);
 
